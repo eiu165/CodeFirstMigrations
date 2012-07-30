3db16edc8fcec94d9fef281213906114f6f6a3d9
eiu165 <eiu165@gmail.com>
2012-07-30 14:49:37 -0500

    task list working

--- Web/Areas/Knockout/Controllers/MainController.cs
+++ Web/Areas/Knockout/Controllers/MainController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace Web.Areas.Knockout.Controllers
 {
@@ -16,5 +17,27 @@ namespace Web.Areas.Knockout.Controllers
             return View();
         }
 
+        public ActionResult Tasks()
+        {
+            return View();
+        }
+
+
+        public JsonResult GetTasks()
+        {  
+            var l = new List<Task>();
+            l.Add(new Task { title = "Wire the money to Panama", isDone = false });
+            l.Add(new Task { title = "Arrange for someone to look after the cat", isDone = true });
+            l.Add(new Task { title = "Get hair dye, beard trimmer, dark glasses", isDone = false });
+            l.Add(new Task { title = "Book taxi to airport", isDone = false });  
+
+
+            return Json(new { title = "aaa", list = l }, JsonRequestBehavior.AllowGet) ;
+        }
+    }
+    public class Task
+    {
+        public string title { get; set; }
+        public bool isDone { get; set; }  
     }
 }
